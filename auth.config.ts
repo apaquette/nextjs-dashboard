4f44ece2d94960a8d39a79c7ceec9be663885756
Alex <alexandre.d.paquette+github@gmail.com>
2024-11-05 15:08:42 +0000

    refactor auth logic

--- auth.config.ts
+++ auth.config.ts
@@ -9,6 +9,17 @@ export const authConfig = {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
 
+            if(!isOnDashboard && isLoggedIn) return Response.redirect(new URL('/dashboard', nextUrl)); //redirect to dashboard if logged in
+
+
+            // true if not on dashboard and not logged in
+            // OR
+            // true if logged in
+            // false otherwise (not logged in)
+            return (!isOnDashboard && !isLoggedIn) || isLoggedIn;
+
+
+
             // // redirect if logged in but not on dashboard
             // if(isLoggedIn && !isOnDashboard)
             //     return Response.redirect(new URL('/dashboard', nextUrl));
@@ -19,13 +30,13 @@ export const authConfig = {
             // return (isOnDashboard && isLoggedIn) || (!isOnDashboard && isLoggedIn)
 
             //TODO: project code to be reworked with better/more intuitive logic
-            if(isOnDashboard){
-                if(isLoggedIn) return true;
-                return false;
-            } else if (isLoggedIn){
-                return Response.redirect(new URL('/dashboard', nextUrl));
-            }
-            return true;
+            // if(isOnDashboard){
+            //     if(isLoggedIn) return true;
+            //     return false; // redirect to login
+            // } else if (isLoggedIn){
+            //     return Response.redirect(new URL('/dashboard', nextUrl));
+            // }
+            // return true;
         },
     },
     providers: [],  //add providers
